4bee8093980e6df0a4466bb4d12621720a31d2c6
SUITE <jesus180497@gmail.com>
2023-04-26 20:36:55 -0500

    tercer commit

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -27,8 +27,25 @@ public class main {
 		System.out.println("ingresar especialidad de Doctor:");
 		
 		String especialidad_doctor = sc.nextLine();
-		//datos del 2 doctor
 		
+		//datos de la cita
+		
+		System.out.println("ingresar datos de cita:");
+		
+		System.out.println("ingresar id de cita:");
+		 int id_cita = sc.nextInt();
+		 
+		 System.out.println("ingresar numero de cita:");
+		 int num_cita = sc.nextInt();
+		 
+		 System.out.println("ingresar fecha de cita:");
+		 int fecha_cita = sc.nextInt();
+		 
+		 System.out.println("ingresar nombre del paciente:");
+		 int nom_paciente_cita = sc.nextInt();
+		 
+		 System.out.println("ingresar fecha de cita:");
+		id_doctor = sc.nextLine();
 		
 	}
 }
